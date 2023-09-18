1dabde687aaf723fdeb2f2b728c9c68d374e4572
Valeria <valeriakirkbride@gmail.com>
2023-09-18 16:51:44 -0600

    restarted much cleaner halfway done

--- src/index.js
+++ src/index.js
@@ -1,61 +1,63 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let form = document.querySelector("#create-task-form");
+  
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    inputToDo(e.target.querySelector("#new-task-description").value, e.target.priority_level.value, e.target.inputDate.value);
+    inputToDo(e.target.querySelector("#new-task-description").value, e.target.priority_level.value);
     form.reset()
   })
 })
 
-function inputToDo(todo, color, input) {
+let form = document.querySelector("#create-task-form");
+
+function inputToDo(todo, color) {
   let li = document.createElement("li");
-  let btn = document.createElement("button");
-  btn.addEventListener("click", handleDelete)
-  btn.textContent = "x";
-  li.textContent = `${todo }` + " " + input ;
-  li.style = "color:" + color;
-  li.appendChild(btn);
+  li.textContent = `${todo}   `;
   document.querySelector("#tasks").appendChild(li);
-  orderList()
-  dateInput()
+  deleteButton(li);
+  priorityColor(li, color);
+  prioritySort();
 }
 
-function handleDelete(e) {
-  e.target.parentNode.remove()
+function deleteButton(li) {
+  let deleteBtn = document.createElement("button");
+  deleteBtn.addEventListener("click", handleComplete());
+  deleteBtn.textContent = "Completed";
+  li.appendChild(deleteBtn);
 }
 
-const priorityMap = {
-  "green": 3,
-  "yellow": 2,
-  "red": 1   
+function handleComplete(e) {
+  e.target.parentNode.remove();
 }
-function orderList () {
-  let list = document.getElementById('tasks');
+
+function prioritySort() {
+  const priorityMap = {
+    "green": 3,
+    "yellow":2,
+    "red": 1
+  }
+  let list = document.getElementById("tasks");
   let items = list.childNodes;
   let itemsArr = [];
-  
-  for (var i in items) {
-    if (items[i].nodeType == 1) { // get rid of the whitespace text nodes
-        itemsArr.push(items[i]);
-    }
-}
 
-  itemsArr.sort(function(a, b) {
+  for (let i in items) {
+    if (items[i].nodeType === 1) {
+      itemsArr.push(items[i]);
+    }
+  }
+  itemsArr.sort(function (a, b) {
     let styleColorA = a.style.color;
     let styleColorB = b.style.color;
     let priorityA = priorityMap[styleColorA];
     let priorityB = priorityMap[styleColorB];
-    return priorityA == priorityB
-            ? 0
-            : (priorityA > priorityB ? 1 : -1);
+    return priorityA === priorityB
+    ? 0
+    : (priorityA > priorityB ? 1 : -1);
   });
-
   for (i = 0; i < itemsArr.length; ++i) {
     list.appendChild(itemsArr[i]);
   }
 }
 
-function dateInput() {
-  let input = document.getElementById("inputDate");
-  return input;
+function priorityColor(li, color){
+  li.style = "color:" + color;
 }
